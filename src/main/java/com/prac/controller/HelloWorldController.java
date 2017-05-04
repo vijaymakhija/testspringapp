158a601ca8c926a0300679db080394b3b9c55cde
vijaymakhija <er.vijaymakhija@gmail.com>
2017-05-03 18:38:47 -0700

    Intial project checkin
    Initial spring app checkin

---
+++ src/main/java/com/prac/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.prac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public ModelAndView welcomeMessage(
+			@RequestParam(value = "name", required = false) String name) {
+		// Name of your jsp file as parameter
+		ModelAndView view = new ModelAndView("hello");
+		view.addObject("name", name);
+		return view;
+	}
+}
